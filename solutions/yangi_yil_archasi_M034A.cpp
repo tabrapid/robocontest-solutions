ac9d43cd213a1ea317ff26e3a5bd51ef79ec70d5
tabrapid <jaloliddinabduganiyev5@gmail.com>
2026-01-06 19:24:36 +0500

    added yangi yil archasi problem

---
+++ solutions/yangi_yil_archasi_M034A.cpp
@@ -0,0 +1,51 @@
+/*
+==================================================
+Problem: [Yangi yil archasi]
+Platform: RoboContest
+Difficulty: [Easy]
+Author: [tabrapid]
+Date: [06/01/2026]
+Time Complexity: [O(1)]
+Space Complexity: [O(1)]
+==================================================
+*/
+
+#include <bits/stdc++.h> // Competitive programming shortcut
+using namespace std;
+
+// Optional macros
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ALL(v) (v).begin(), (v).end()
+
+// Optional typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int main()
+{
+    FAST_IO;
+
+    int tests;
+    std::cin >> tests;
+
+    while (tests--)
+    {
+        int height, diameter, branches;
+
+        std::cin >> height >> diameter >> branches;
+
+        if ((height >= 200 && height <= 300) && (diameter >= 50) && (branches >= 150))
+        {
+            std::cout << "Yes" << std::endl;
+        }
+        else
+        {
+            std::cout << "No" << std::endl;
+        }
+    }
+
+    return 0;
+}
