0b392d1d98bc2ea28eb8202acec8de38e3c604b6
tabrapid <jaloliddinabduganiyev5@gmail.com>
2026-01-06 17:47:56 +0500

    added gulliver problem

---
+++ solutions/gulliver_M041A.cpp
@@ -0,0 +1,37 @@
+/*
+==================================================
+Problem: [Gulliver]
+Platform: RoboContest
+Difficulty: [Easy]
+Author: [tabrapid]
+Date: [06/01/2026]
+Time Complexity: [O(1)]
+Space Complexity: [O(1)]
+==================================================
+*/
+
+#include <bits/stdc++.h> // Competitive programming shortcut
+using namespace std;
+
+// Optional macros
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ALL(v) (v).begin(), (v).end()
+
+// Optional typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int main()
+{
+    FAST_IO;
+
+    ll a, b;
+    std::cin >> a >> b;
+
+    std::cout << (a * a) * b << std::endl;
+
+    return 0;
+}
