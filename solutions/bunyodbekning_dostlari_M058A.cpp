3017dbf2e851398e9b37e238d024dfcf88189ba5
tabrapid <jaloliddinabduganiyev5@gmail.com>
2026-01-06 17:59:23 +0500

    added bunyodbekning do'stlari problem

---
+++ solutions/bunyodbekning_dostlari_M058A.cpp
@@ -0,0 +1,37 @@
+/*
+==================================================
+Problem: [Bunyodbekning do'stlari]
+Platform: RoboContest
+Difficulty: [Easy]
+Author: [tabrapid]
+Date: [06/01/2026]
+Time Complexity: [O(1)]
+Space Complexity: [O(1)]
+==================================================
+*/
+
+#include <bits/stdc++.h> // Competitive programming shortcut
+using namespace std;
+
+// Optional macros
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ALL(v) (v).begin(), (v).end()
+
+// Optional typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int main()
+{
+    FAST_IO;
+
+    ll a;
+    std::cin >> a;
+
+    std::cout << a - 3 << std::endl;
+
+    return 0;
+}
