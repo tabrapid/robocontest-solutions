9880c28aa9aeb5fffc6b97f6cff3f36bf443272b
tabrapid <jaloliddinabduganiyev5@gmail.com>
2026-01-06 17:21:54 +0500

    added ajoyib oyin problem

---
+++ solutions/ajoyib_oyin_R059E.cpp
@@ -0,0 +1,37 @@
+/*
+==================================================
+Problem: [Ajoyib o'yin]
+Platform: RoboContest
+Difficulty: [Easy]
+Author: [tabrapid]
+Date: [06/01/2026]
+Time Complexity: [O(1)]
+Space Complexity: [O(1)]
+==================================================
+*/
+
+#include <bits/stdc++.h> // Competitive programming shortcut
+using namespace std;
+
+// Optional macros
+#define FAST_IO                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+#define ALL(v) (v).begin(), (v).end()
+
+// Optional typedefs
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+int main()
+{
+    FAST_IO;
+
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << std::abs(a - b) + 1 << std::endl;
+
+    return 0;
+}
