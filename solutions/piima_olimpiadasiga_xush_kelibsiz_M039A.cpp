404168a0b2449801db1c9752bd58e4945e907b9e
Jaloliddin Abduganiyev <jaloliddinabduganiyev5@gmail.com>
2026-01-08 23:02:00 +0500

    added new problem

---
+++ solutions/piima_olimpiadasiga_xush_kelibsiz_M039A.cpp
@@ -0,0 +1,39 @@
+/*
+==================================================
+Problem: [PIIMA olimpiadasiga xush kelibsiz 2]
+Platform: RoboContest
+Author: [tabrapid]
+Date: [06/01/2026]
+==================================================
+*/
+
+#include <bits/stdc++.h> // Competitive programming shortcut
+
+// Optional macros
+#define FAST_IO                       \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);
+#define ALL(v) (v).begin(), (v).end()
+
+// Optional typedefs
+typedef long long ll;
+typedef std::vector<int> vi;
+typedef std::pair<int, int> pii;
+
+int main()
+{
+    FAST_IO;
+    
+    std::string name;
+    std::cin >> name;
+    
+    name[0] = std::toupper(name[0]);
+
+    std::cout << "PIIMA olimpiadasiga xush kelibsiz, "<< name << '!' <<  std::endl;
+
+    return 0;
+}
+
+
+
+
